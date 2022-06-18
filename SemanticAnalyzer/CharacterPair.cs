0e12141a6b24ba2c2237263cf4f1ae8ce8835b7f
officejellobit <office@jellobit.at>
2022-06-18 11:49:04 +0200

    Helper class for CharacterPair

---
+++ SemanticAnalyzer/CharacterPair.cs
@@ -0,0 +1,17 @@
+﻿namespace SemanticAnalyzer
+{
+    /// <summary>
+    /// helper class for holding the combination of opening and closing character
+    /// </summary>
+    internal class CharacterPair
+    {
+        public Char OpeningChar { get; }
+        public Char ClosingChar { get; }
+
+        public CharacterPair(Char openingChar, Char closingChar)
+        {
+            OpeningChar = openingChar;
+            ClosingChar = closingChar;
+        }
+    }
+}
